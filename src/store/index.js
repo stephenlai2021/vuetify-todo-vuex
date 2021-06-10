6ce37f40a6ca7e5c77442aefdca0ccfee98da2ac
stephenlaichaowen <stephenlaitest@gmail.com>
2021-06-10 15:46:12 +0800

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,39 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { db } from "../database/localbase";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    counter: 0,
+    tasks: [], 
+    task: {}
+  },
+  mutations: {
+    getTasks(state) {
+      db.collection("todos")
+        .get()
+        .then((tasks) => {
+          state.tasks = tasks;
+        });
+    },
+    addTask(state, task) {
+      db.collection('todos').doc(task.id).set(task)
+      state.tasks.push(task)
+    },
+    deleteTask(state, task) {
+      db.collection("todos").doc(task.id).delete();
+      state.tasks = state.tasks.filter(item => item.id !== task.id)
+    },
+    toggleTask(state, payload) {
+      state.task = state.tasks.find(item => item.id === payload.id)
+      
+      db.collection("todos").doc(payload.id).update({ done: !state.task.done });
+      state.task.done = !state.task.done
+    },
+  },
   actions: {},
+  getters: {},
   modules: {},
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-text-field
+      v-model="newTask"
+      class="pa-3"
+      outlined
+      label="Add task"
+      append-icon="mdi-plus"
+      hide-details
+      clearable
+      @click:append="addTask"
+      @keyup.enter="addTask"
+    ></v-text-field>
+
+    <v-list flat class="pt-0">
+      <div v-for="task in $store.state.tasks" :key="task.id">
+        <v-list-item
+          @click="toggleTask(task)"
+          :class="{ 'blue lighten-5': task.done }"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+                >{{ task.title }}</v-list-item-title
+              >
+            </v-list-item-content>
+
+            <v-list-item-action v-if="task.done">
+            <!-- <v-list-item-action> -->
+              <v-btn icon @click.stop="deleteTask(task)">
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      newTask: "",
+      tasks: [],
+    };
+  },
+  created() {
+    this.$store.commit('getTasks')
+  },
+  methods: {
+    toggleTask(task) {
+      this.$store.commit('toggleTask', task)
+    },
+    deleteTask(task) {
+      this.$store.commit('deleteTask', task)
+    },
+    addTask() {     
+      const task = {
+        title: this.newTask,
+        done: false,
+        createdAt: new Date().toLocaleString(),
+        id: Date.now().toString()
+      }
+      this.$store.commit('addTask', task)
+      this.newTask = "";
+    },
   },
 };
 </script>
+
+<style scoped>
+.v-list-item {
+  height: 60px;
+}
+</style>
